ca60538e902f8ac85491041048e365f765260c93
Steven Hong <stevenfzero07@gmail.com>
2015-02-10 22:34:17 -0800

    added previous improvements to blocking, using block size 32

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -3,7 +3,7 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_SIZE 192
+#define BLOCK_SIZE 32
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -26,7 +26,7 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
       //C[i+j*lda] = cij;
     }*/
 
-  int m = 0;
+  /*int m = 0;
   if (M%2 != 0)
     m = M-1;
   else
@@ -54,6 +54,265 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
       }
       //C[i+j*n] = cij;
     }
+  }*/
+
+  int M2, N4, K_unroll, aindex, bindex, bindex1, bindex2, bindex3, bindex4, 
+  cindex, cindex1, cindex2, cindex3, cindex4, i, j, k;
+  //__m128d a, b, b1, b2, b3, b4, c, c1, c2, c3, c4;
+  __m128d b, b1, b2, b3, b4;
+  register __m128d a, c, c1, c2, c3, c4;
+
+  if (M%2 != 0)
+    M2 = M-1;
+  else
+    M2 = M;
+
+  N4 = N - (N % 4);
+  K_unroll = K - (K % 4);
+
+  j = 0;
+  for (; j < N4; j+=4) {
+    cindex1 = j*lda;
+    cindex2 = (j+1)*lda;
+    cindex3 = (j+2)*lda;
+    cindex4 = (j+3)*lda;
+    //for (int i = 0; i < n; ++i) {
+    i = 0;
+    for (; i < M2; i+=2) {
+      //double cij = C[i+j*n];
+      //int aindex = k*n;
+      //int cindex = j*n;
+      //int bindex = k+cindex;
+      //__m128d bcol = _mm_load1_pd(B+bindex);
+      c1 = _mm_loadu_pd(C+i+cindex1);
+      c2 = _mm_loadu_pd(C+i+cindex2);
+      c3 = _mm_loadu_pd(C+i+cindex3);
+      c4 = _mm_loadu_pd(C+i+cindex4);
+      //int k = 0;
+      //for(; k < N; k = k+2) {
+      k = 0;
+      for (; k < K_unroll; k+=4) {
+         //cij += A[i+k*n] * B[k+j*n];
+         //C[i+cindex] += A[i+aindex] * B[bindex];
+        aindex = k*lda;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+
+        aindex = (k+1)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+        
+        aindex = (k+2)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+
+        aindex = (k+3)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+      }
+
+      for (; k < K; ++k) {
+        aindex = k*lda;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+      }
+      //for (; i < n; ++i) {
+        //C[i+cindex] += A[i+aindex] * B[bindex];
+      //}
+      //C[i+j*n] = cij;
+      _mm_storeu_pd(C+i+cindex1, c1);
+      _mm_storeu_pd(C+i+cindex2, c2);
+      _mm_storeu_pd(C+i+cindex3, c3);
+      _mm_storeu_pd(C+i+cindex4, c4);
+    }
+    for (; i < M; ++i) {
+      //for (int k = 0; k < n; ++k) {
+        //C[i+cindex] += A[i+k*n] * B[k+j*n];
+      //}
+
+      k = 0;
+      for (; k < K_unroll; k+=4) {
+        aindex = k*lda;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+1)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+2)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+3)*lda;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+      }
+
+      for (; k < K; ++k) {
+        aindex = k*lda;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+      }
+    }
+  }
+
+  for (; j < N; ++j) {
+    cindex = j*lda;
+
+    i = 0;
+    for (; i < M2; i+=2) {
+      c = _mm_loadu_pd(C+i+cindex);
+
+      k = 0;
+      for (; k < K_unroll; k+=4) {
+        aindex = k*lda;
+        bindex = k+cindex;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+1)*lda;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+2)*lda;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+3)*lda;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+      }
+
+      for (; k < K; k++) {
+         //cij += A[i+k*n] * B[k+j*n];
+         //C[i+cindex] += A[i+aindex] * B[bindex];
+        aindex = k*lda;
+        bindex = k+cindex;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+      }
+      
+      //C[i+j*n] = cij;
+      _mm_storeu_pd(C+i+cindex, c);
+    }
+
+    for (; i < M; ++i) {
+      k = 0;
+      /*for (; k < N_unroll; k+=4) {
+        C[i+cindex] += A[i+k*n] * B[k+j*n];
+        C[i+cindex] += A[i+(k+1)*n] * B[k+1+j*n];
+        C[i+cindex] += A[i+(k+2)*n] * B[k+2+j*n];
+        C[i+cindex] += A[i+(k+3)*n] * B[k+3+j*n];
+      }*/
+
+      for (; k < K; ++k) {
+        C[i+cindex] += A[i+k*lda] * B[k+j*lda];
+      }
+    }
   }
 }
 
@@ -66,9 +325,9 @@ void square_dgemm (int lda, double* A, double* B, double* C)
   /* For each block-row of A */ 
   for (int j = 0; j < lda; j += BLOCK_SIZE)
     /* For each block-column of B */
-    for (int k = 0; k < lda; k += BLOCK_SIZE)
+    for (int i = 0; i < lda; i += BLOCK_SIZE)
       /* Accumulate block dgemms into block of C */
-      for (int i = 0; i < lda; i += BLOCK_SIZE)
+      for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
 	       /* Correct block dimensions if block "goes off edge of" the matrix */
 	       int M = min (BLOCK_SIZE, lda-i);
--- dgemm-naive.c
+++ dgemm-naive.c
@@ -8,7 +8,8 @@ const char* dgemm_desc = "Naive, three-loop dgemm.";
  * On exit, A and B maintain their input values. */    
 void square_dgemm (int n, double* A, double* B, double* C)
 {
-  int N2, N4, aindex, bindex, bindex1, bindex2, bindex3, bindex4, cindex, cindex1, cindex2, cindex3, cindex4, i, j;
+  int N2, N4, N_unroll, aindex, bindex, bindex1, bindex2, bindex3, bindex4, 
+  cindex, cindex1, cindex2, cindex3, cindex4, i, j, k;
   //__m128d a, b, b1, b2, b3, b4, c, c1, c2, c3, c4;
   __m128d b, b1, b2, b3, b4;
   register __m128d a, c, c1, c2, c3, c4;
@@ -19,6 +20,7 @@ void square_dgemm (int n, double* A, double* B, double* C)
     N2 = n;
 
   N4 = n - (n % 4);
+  N_unroll = N4;
 
   j = 0;
   for (; j < N4; j+=4) {
@@ -40,7 +42,8 @@ void square_dgemm (int n, double* A, double* B, double* C)
       c4 = _mm_loadu_pd(C+i+cindex4);
       //int k = 0;
       //for(; k < N; k = k+2) {
-      for (int k = 0; k < n; k++) {
+      k = 0;
+      for (; k < N_unroll; k+=4) {
 	       //cij += A[i+k*n] * B[k+j*n];
 	       //C[i+cindex] += A[i+aindex] * B[bindex];
         aindex = k*n;
@@ -53,13 +56,77 @@ void square_dgemm (int n, double* A, double* B, double* C)
         b2 = _mm_load1_pd(B+bindex2);
         b3 = _mm_load1_pd(B+bindex3);
         b4 = _mm_load1_pd(B+bindex4);
-        //__m128d c = _mm_loadu_pd(C+i+cindex);
-        //c = _mm_add_pd(c, _mm_mul_pd(a, bcol));
+        
         c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
         c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
         c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
         c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
-        //_mm_storeu_pd(C+i+cindex, c);
+
+        aindex = (k+1)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+        
+        aindex = (k+2)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+
+        aindex = (k+3)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
+      }
+
+      for (; k < n; ++k) {
+        aindex = k*n;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+        a = _mm_loadu_pd(A+i+aindex);
+        b1 = _mm_load1_pd(B+bindex1);
+        b2 = _mm_load1_pd(B+bindex2);
+        b3 = _mm_load1_pd(B+bindex3);
+        b4 = _mm_load1_pd(B+bindex4);
+        
+        c1 = _mm_add_pd(c1, _mm_mul_pd(a, b1));
+        c2 = _mm_add_pd(c2, _mm_mul_pd(a, b2));
+        c3 = _mm_add_pd(c3, _mm_mul_pd(a, b3));
+        c4 = _mm_add_pd(c4, _mm_mul_pd(a, b4));
       }
       //for (; i < n; ++i) {
         //C[i+cindex] += A[i+aindex] * B[bindex];
@@ -75,7 +142,54 @@ void square_dgemm (int n, double* A, double* B, double* C)
         //C[i+cindex] += A[i+k*n] * B[k+j*n];
       //}
 
-      for (int k = 0; k < n; ++k) {
+      k = 0;
+      for (; k < N_unroll; k+=4) {
+        aindex = k*n;
+        bindex1 = k+cindex1;
+        bindex2 = k+cindex2;
+        bindex3 = k+cindex3;
+        bindex4 = k+cindex4;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+1)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+2)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+
+        aindex = (k+3)*n;
+        bindex1 += 1;
+        bindex2 += 1;
+        bindex3 += 1;
+        bindex4 += 1;
+
+        C[i+cindex1] += A[i+aindex] * B[bindex1];
+        C[i+cindex2] += A[i+aindex] * B[bindex2];
+        C[i+cindex3] += A[i+aindex] * B[bindex3];
+        C[i+cindex4] += A[i+aindex] * B[bindex4];
+      }
+
+      for (; k < n; ++k) {
         aindex = k*n;
         bindex1 = k+cindex1;
         bindex2 = k+cindex2;
@@ -92,37 +206,62 @@ void square_dgemm (int n, double* A, double* B, double* C)
 
   for (; j < n; ++j) {
     cindex = j*n;
-    //for (int i = 0; i < n; ++i) {
+
     i = 0;
     for (; i < N2; i+=2) {
-      //double cij = C[i+j*n];
-      //int aindex = k*n;
-      //int cindex = j*n;
-      //int bindex = k+cindex;
-      //__m128d bcol = _mm_load1_pd(B+bindex);
       c = _mm_loadu_pd(C+i+cindex);
-      //int k = 0;
-      //for(; k < N; k = k+2) {
-      for (int k = 0; k < n; k++) {
+
+      k = 0;
+      for (; k < N_unroll; k+=4) {
+        aindex = k*n;
+        bindex = k+cindex;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+1)*n;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+2)*n;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+
+        aindex = (k+3)*n;
+        bindex += 1;
+        a = _mm_loadu_pd(A+i+aindex);
+        b = _mm_load1_pd(B+bindex);
+        c = _mm_add_pd(c, _mm_mul_pd(a, b));
+      }
+
+      for (; k < n; k++) {
          //cij += A[i+k*n] * B[k+j*n];
          //C[i+cindex] += A[i+aindex] * B[bindex];
         aindex = k*n;
         bindex = k+cindex;
         a = _mm_loadu_pd(A+i+aindex);
         b = _mm_load1_pd(B+bindex);
-        //__m128d c = _mm_loadu_pd(C+i+cindex);
-        //c = _mm_add_pd(c, _mm_mul_pd(a, bcol));
         c = _mm_add_pd(c, _mm_mul_pd(a, b));
-        //_mm_storeu_pd(C+i+cindex, c);
       }
-      //for (; i < n; ++i) {
-        //C[i+cindex] += A[i+aindex] * B[bindex];
-      //}
+      
       //C[i+j*n] = cij;
       _mm_storeu_pd(C+i+cindex, c);
     }
+
     for (; i < n; ++i) {
-      for (int k = 0; k < n; ++k) {
+      k = 0;
+      /*for (; k < N_unroll; k+=4) {
+        C[i+cindex] += A[i+k*n] * B[k+j*n];
+        C[i+cindex] += A[i+(k+1)*n] * B[k+1+j*n];
+        C[i+cindex] += A[i+(k+2)*n] * B[k+2+j*n];
+        C[i+cindex] += A[i+(k+3)*n] * B[k+3+j*n];
+      }*/
+
+      for (; k < n; ++k) {
         C[i+cindex] += A[i+k*n] * B[k+j*n];
       }
     }
